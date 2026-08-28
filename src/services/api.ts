bf88ec4625aa697d3e26ecf718986532af85e8d2
Furkan Şahin <sahinf19@itu.edu.tr>
2026-08-28 15:49:56 +0300

    feat: add request interceptor for token injection

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,4 +8,19 @@ const api = axios.create({
     },
 });
 
+// request interceptor to add the JWT token to the Authorization header
+api.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+        const token = localStorage.getItem("token"); // TODO: replace with secure storage mechanism
+        // if both token and config header exist, add the token to the Authorization header
+        if (token && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config; // send the updated request config to the backend
+    },
+    (error: AxiosError) => {
+        return Promise.reject(error);
+    }
+);
+
 export default api;
