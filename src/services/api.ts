662af945adcb4a7f085f4b5e73fc5da9f3aeee3f
Furkan Şahin <sahinf19@itu.edu.tr>
2026-08-28 14:47:07 +0300

    feat: create axios instance for interceptors

---
+++ src/services/api.ts
@@ -0,0 +1,11 @@
+import axios, { type AxiosError, type InternalAxiosRequestConfig } from "axios";
+
+
+const api = axios.create({
+    baseURL: "http://localhost:8080/api",
+    headers: {
+    "Content-Type": "application/json",
+    },
+});
+
+export default api;
